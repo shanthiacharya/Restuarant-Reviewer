ee59810b6553d255ad5fdfa3b2000e3928152223
shanthiacharya <ShanthiAcharya@OSXSACHAMBP15.local>
2016-07-05 07:16:48 -0700

    Updates

--- js/app.js
+++ js/app.js
@@ -1,3 +1,67 @@
+function getRestaurantJSONArray(){
+    var restArr = new Array;
+    var xmlhttp = new XMLHttpRequest();
+    var url = "../data/restuarant.json";
+    xmlhttp.onreadystatechange = function() {
+    if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            restArr = JSON.parse(xmlhttp.responseText);
+        }
+    }
+    xmlhttp.open("GET", url, true);
+    xmlhttp.send();
+    return restArr;
+}
+
+
+$('#category input[type=checkbox]').click(function(){
+    // code goes here
+
+
+
+
+    if ($(this).is(':checked')) {
+    var currval = $(this).val()
+    console.log($(this).val());
+
+    var restArr = new Array;
+    var xmlhttp = new XMLHttpRequest();
+    var url = "../data/restuarant.json";
+    xmlhttp.onreadystatechange = function() {
+    if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            restArr = JSON.parse(xmlhttp.responseText);
+            console.log(restArr);
+            filteredcuisine =   restArr.filter (function (value) {
+            if(value.cuisine == currval) {
+
+                  return true
+                } else {
+
+                  return false;
+                }
+              });
+
+            console.log(filteredcuisine);
+          //  displayResturantList(filteredcuisine);
+            // Grab the template script
+            var theTemplateScript = $("#restuarant-template").html();
+            // console.log(theTemplateScript)
+            // Compile the template
+            var theTemplate = Handlebars.compile(theTemplateScript);
+
+            // Pass our data to the template
+            var theCompiledHtml = theTemplate(filteredcuisine);
+
+            // Add the compiled html to the page
+            $('.content-placeholder').html(theCompiledHtml);
+
+        }
+    }
+    xmlhttp.open("GET", url, true);
+    xmlhttp.send();
+
+
+  }
+});
 
 var myGreatFunction = function(someValue) {
     // Work with that value
@@ -8,29 +72,15 @@ var myGreatFunction = function(someValue) {
   // $(location).href ="restuarantdetail.html"
 }
 
-// (function() {
-//     'use strict';
-//     var dialogButton = document.querySelector('.dialog-button');
-//     var dialog = document.querySelector('#dialog');
-//     if (! dialog.showModal) {
-//       dialogPolyfill.registerDialog(dialog);
-//     }
-//     dialogButton.addEventListener('click', function() {
-//        dialog.showModal();
-//     });
-//     dialog.querySelector('button:not([disabled])')
-//     .addEventListener('click', function() {
-//       dialog.close();
-//     });
-//   }());
-
-
 Handlebars.registerHelper('json', function(context) {
   //return JSON.stringify(context);
 //  var str = JSON.stringify(context).replace(/"/g, '&quot;');
 //  console.log(str);
   return JSON.stringify(context).replace(/"/g, '&quot;');
 });
+
+
+
 
 
 $(document).ready(function(){
@@ -79,21 +129,13 @@ $(document).ready(function(){
      xmlhttp.open("GET", url, true);
      xmlhttp.send();
 
-     var dialogButton = document.querySelector('.dialog-button');
-     var dialog = document.querySelector('#dialog');
-     if (! dialog.showModal) {
-       dialogPolyfill.registerDialog(dialog);
-     }
-     dialogButton.addEventListener('click', function() {
-       console.log('fab clicked')
-        dialog.showModal();
-     });
-     dialog.querySelector('button:not([disabled])')
-     .addEventListener('click', function() {
-       dialog.close();
-     });
+
 
    }
+
+
+
+
 
 
   function displayResturantList(context) {
@@ -142,4 +184,56 @@ $(document).ready(function(){
   }
 
 
+
+
 });
+
+
+
+function showModal(){
+
+  var dialogButton = document.querySelector('.dialog-button');
+
+  var dialog = document.querySelector('#dialog');
+
+  dialog.querySelector('button:not([disabled])')
+  .addEventListener('click', function() {
+    dialog.close();
+  });
+  if (! dialog.showModal) {
+    dialogPolyfill.registerDialog(dialog);
+  }
+  dialog.showModal();
+
+}
+
+$(".fab").click(function() {
+      $(".mdl-layout__drawer-right").addClass("active");
+      $(".mdl-layout__obfuscator-right").addClass("ob-active");
+    });
+
+    $(".mdl-layout__obfuscator-right").click(function() {
+      $(".mdl-layout__drawer-right").removeClass("active");
+      $(".mdl-layout__obfuscator-right").removeClass("ob-active");
+});
+
+$(".close").click(function() {
+      $(".mdl-layout__drawer-right").removeClass("active");
+      $(".mdl-layout__obfuscator-right").removeClass("ob-active");
+    });
+
+    $(".mdl-layout__obfuscator-right").click(function() {
+      $(".mdl-layout__drawer-right").addClass("active");
+      $(".mdl-layout__obfuscator-right").addClass("ob-active");
+});
+
+//
+// $('#chkbox1').click(function() {
+//        if ($(this).is(':checked')) {
+//         console.log("Checkbox clicked")
+//       }
+// });
+
+// $('#chkbox1').click(function() {
+//     console.log($(this).val());
+// });
